c2d5ae298f4ef7180b49a5dcaad5fda70961dd2f
ChevvyChelios <souravsarania2@gmail.com>
2024-12-24 00:23:44 +0530

    alukhaa

--- temp.c
+++ temp.c
@@ -1,34 +1,10 @@
-#include <stdio.h>
 
-#define MAX 20 // Maximum size of the matrix
-
-void printTriangle(int arr[20][20], int n){
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i>=j)
-                printf("%d ", arr[i][j]);
-            else
-                printf("0 ");
-        }
-        printf("\n");
-    }
-}
-
-int main() {
-    int n, i, j;
-    int matrix[MAX][MAX];
-    printf("Enter size: ");
-    scanf("%d", &n);
-
-    printf("Enter the elements of the lower triangular matrix:\n");
-    for (i = 0; i < 5; i++) {
-        for (j = 0; j < 5; j++) {
-            printf("Element [%d][%d]: ", i, j);
-            scanf("%d", &matrix[i][j]);
-        }
-    }
-
-    printTriangle(matrix,n);
-
-    return 0;
+#include<stdio.h>
+void main(){
+    int a, *b=&a, **c=&b;
+    a = 15;
+    **c = 25;
+    *b = **c;
+    printf("%d %d %d", a, *b, **c);
+    printf("\n");
 }
